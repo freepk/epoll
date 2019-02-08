64b6ec539c7d1d427363b43079e388f0cb6f881a
Pavel Chabinsky <freepk@mail.ru>
2019-02-08 11:10:13 +0300

    ...

---
+++ fasthttp.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/valyala/fasthttp"
+)
+
+func main() {
+	fasthttp.ListenAndServe(":8080", func(ctx *fasthttp.RequestCtx) {
+		ctx.WriteString("Hello World!")
+	})
+}
