0b61ddfbe4827f7c1264b9550c0b07dde73c5d75
freepk <freepk@mail.ru>
2019-02-27 11:13:32 +0300

    Update epoll.c

--- epoll.c
+++ epoll.c
@@ -3,19 +3,25 @@
 #include <netinet/tcp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <pthread.h>
 
-#define MAX_EVENTS 128
+#define MAX_EVENTS 2048
 
-static char reply[50] = "HTTP/1.1 200 OK\r\n"
-	"Content-Length: 11\r\n"
+int defaultPort = 8888;
+char defaultResponse[90] = "HTTP/1.1 202 OK\r\n"
+	"Server: Techlab\r\n"
+	"Content-Type: application/json\r\n"
+	"Content-Length: 2\r\n"
 	"\r\n"
-	"Hello World";
+	"{}";
+char getMethod[3] = "GET";
+char postMethod[4] = "POST";
 
-void handle_input(int epollfd, int fd) {
+void handleInput(int epollfd, int fd) {
 	int n;
 	char buf[16384];
 
@@ -30,14 +36,17 @@ void handle_input(int epollfd, int fd) {
 		close(fd);
 		return;
 	}
-	n = write(fd, reply, 50);
+	if (strncmp(buf, getMethod, 3) != 0 && strncmp(buf, postMethod, 4) != 0) {
+		return;
+	}
+	n = write(fd, defaultResponse, 90);
 	if (n == -1) {
 		perror("write failed");
 		exit(EXIT_FAILURE);
 	}
 }
 
-void *start_loop(void *dummy) {
+void *startLoop(void *dummy) {
 	int listen_sock, client_sock;
 	int one, n, i;
 	int epollfd;
@@ -47,7 +56,7 @@ void *start_loop(void *dummy) {
 	one = 1;
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(8888);
+	addr.sin_port = htons(defaultPort);
 
 	listen_sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
 	if (listen_sock == -1) {
@@ -55,7 +64,7 @@ void *start_loop(void *dummy) {
 		exit(EXIT_FAILURE);
 	}
 	if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEPORT, &one, sizeof(one)) == -1) {
-		perror("setsockopt failed");
+		perror("setsockopt SO_REUSEPORT failed");
 		exit(EXIT_FAILURE);
 	}
 	if (bind(listen_sock, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
@@ -78,7 +87,7 @@ void *start_loop(void *dummy) {
 		exit(EXIT_FAILURE);
 	}
 	for (;;) {
-		n = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+		n = epoll_wait(epollfd, events, MAX_EVENTS, 0);
 		if (n == -1) {
 			perror("epoll_wait failed");
 			exit(EXIT_FAILURE);
@@ -90,14 +99,22 @@ void *start_loop(void *dummy) {
 					perror("accept failed");
 					exit(EXIT_FAILURE);
 				}
-				event.events = EPOLLIN | EPOLLET;
+				if (setsockopt(client_sock, IPPROTO_TCP, TCP_NODELAY, &one, sizeof(one)) == -1) {
+					perror("setsockopt TCP_NODELAY failed");
+					exit(EXIT_FAILURE);
+				}
+				if (setsockopt(client_sock, IPPROTO_TCP, TCP_QUICKACK, &one, sizeof(one)) == -1) {
+					perror("setsockopt TCP_QUICKACK failed");
+					exit(EXIT_FAILURE);
+				}
+				event.events = EPOLLIN /*| EPOLLET*/;
 				event.data.fd = client_sock;
 				if (epoll_ctl(epollfd, EPOLL_CTL_ADD, client_sock, &event) == -1) {
 					perror("epoll_ctl failed");
 					exit(EXIT_FAILURE);
 				}
 			} else {
-				handle_input(epollfd, events[i].data.fd);
+				handleInput(epollfd, events[i].data.fd);
 			}
 		}
 	}
@@ -105,16 +122,21 @@ void *start_loop(void *dummy) {
 }
 
 int main() {
-	pthread_t thr1, thr2;
-	if (pthread_create(&thr1, NULL, start_loop, NULL) != 0) {
+	pthread_t thr1, thr2, thr3;
+	if (pthread_create(&thr1, NULL, startLoop, NULL) != 0) {
 		perror("pthread_create failed");
 		exit(EXIT_FAILURE);
 	}
-	if (pthread_create(&thr2, NULL, start_loop, NULL) != 0) {
-                perror("pthread_create failed");
-                exit(EXIT_FAILURE);
+	if (pthread_create(&thr2, NULL, startLoop, NULL) != 0) {
+        perror("pthread_create failed");
+        exit(EXIT_FAILURE);
+	}
+	if (pthread_create(&thr3, NULL, startLoop, NULL) != 0) {
+        perror("pthread_create failed");
+        exit(EXIT_FAILURE);
 	}
 	pthread_join(thr1, NULL);
 	pthread_join(thr2, NULL);
+	pthread_join(thr3, NULL);
 	return 0;
 }
