f8251afb7bdda9c674c2d3f3a3fe66c30b5e08a4
Pavel Chabinsky <freepk@mail.ru>
2019-03-18 02:28:32 +0300

    Refactor

--- epoll.c
+++ epoll.c
@@ -1,143 +1,136 @@
 #define _GNU_SOURCE
 #include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <pthread.h>
 
-#define MAX_EVENTS 2048
+#define MAX_EVENTS 16384
+#define SERVER_PORT 80
 
-int defaultPort = 8888;
+char defaultResponse[] = "HTTP/1.1 200 OK\r\n"
+                         "Content-Length: 2\r\n"
+                         "\r\n"
+                         "{}";
 
-char defaultResponse[50] = "HTTP/1.1 200 OK\r\n"
-        "Content-Length: 11\r\n"
-        "\r\n"
-        "Hello World";
+int defaultResponseLen = 40;
 
-char getMethod[3] = "GET";
+void handleInput(int epollfd, int fd)
+{
+    int n;
+    char buf[16384];
 
-char postMethod[4] = "POST";
-
-void handleInput(int epollfd, int fd) {
-	int n;
-	char buf[16384];
-
-	n = read(fd, buf, sizeof(buf));
-	if (n == 0) {
-		epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, NULL);
-		close(fd);
-		return;
-	}
-	if (n == -1) {
-		epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, NULL);
-		close(fd);
-		return;
-	}
-	if (strncmp(buf, getMethod, 3) != 0 && strncmp(buf, postMethod, 4) != 0) {
-		return;
-	}
-	n = write(fd, defaultResponse, 50);
-	if (n == -1) {
-		perror("write failed");
-		exit(EXIT_FAILURE);
-	}
+    n = recv(fd, buf, sizeof(buf), 0);
+    if (n == 0) {
+        epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, NULL);
+        close(fd);
+        return;
+    }
+    if (n == -1) {
+        epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, NULL);
+        close(fd);
+        return;
+    }
+    if (strncmp(buf, "GET", 3) != 0 && strncmp(buf, "POST", 4) != 0) {
+        return;
+    }
+    n = send(fd, defaultResponse, defaultResponseLen, 0);
+    if (n == -1) {
+        perror("send failed");
+        exit(EXIT_FAILURE);
+    }
 }
 
-void *startLoop(void *dummy) {
-	int listen_sock, client_sock;
-	int one, n, i;
-	int epollfd;
-	struct sockaddr_in addr;
-	struct epoll_event event, events[MAX_EVENTS];
+void* startLoop(void* dummy)
+{
+    int listen_sock, client_sock;
+    int one, n, i;
+    int epollfd;
+    struct sockaddr_in addr;
+    struct epoll_event event, events[MAX_EVENTS];
 
-	one = 1;
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(defaultPort);
+    one = 1;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(SERVER_PORT);
 
-	listen_sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-	if (listen_sock == -1) {
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
-	if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEPORT, &one, sizeof(one)) == -1) {
-		perror("setsockopt SO_REUSEPORT failed");
-		exit(EXIT_FAILURE);
-	}
-	if (bind(listen_sock, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(listen_sock, SOMAXCONN) == -1) {
-		perror("listen failed");
-		exit(EXIT_FAILURE);
-	}
-	epollfd = epoll_create1(0);
-	if (epollfd == -1) {
-		perror("epoll_create1 failed");
-		exit(EXIT_FAILURE);
-	}
-	event.events = EPOLLIN;
-	event.data.fd = listen_sock;
-	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sock, &event) == -1) {
-		perror("epoll_ctl failed");
-		exit(EXIT_FAILURE);
-	}
-	for (;;) {
-		n = epoll_wait(epollfd, events, MAX_EVENTS, 0);
-		if (n == -1) {
-			perror("epoll_wait failed");
-			exit(EXIT_FAILURE);
-		}
-		for (i = 0; i < n; i++) {
-			if (events[i].data.fd == listen_sock) {
-				client_sock = accept4(listen_sock, NULL, NULL, SOCK_NONBLOCK);
-				if (client_sock == -1) {
-					perror("accept failed");
-					exit(EXIT_FAILURE);
-				}
-				if (setsockopt(client_sock, IPPROTO_TCP, TCP_NODELAY, &one, sizeof(one)) == -1) {
-					perror("setsockopt TCP_NODELAY failed");
-					exit(EXIT_FAILURE);
-				}
-				if (setsockopt(client_sock, IPPROTO_TCP, TCP_QUICKACK, &one, sizeof(one)) == -1) {
-					perror("setsockopt TCP_QUICKACK failed");
-					exit(EXIT_FAILURE);
-				}
-				event.events = EPOLLIN /*| EPOLLET*/;
-				event.data.fd = client_sock;
-				if (epoll_ctl(epollfd, EPOLL_CTL_ADD, client_sock, &event) == -1) {
-					perror("epoll_ctl failed");
-					exit(EXIT_FAILURE);
-				}
-			} else {
-				handleInput(epollfd, events[i].data.fd);
-			}
-		}
-	}
-	return NULL;
+    listen_sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    if (listen_sock == -1) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEPORT, &one, sizeof(one)) == -1) {
+        perror("setsockopt SO_REUSEPORT failed");
+        exit(EXIT_FAILURE);
+    }
+    if (bind(listen_sock, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(listen_sock, SOMAXCONN) == -1) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+    epollfd = epoll_create1(0);
+    if (epollfd == -1) {
+        perror("epoll_create1 failed");
+        exit(EXIT_FAILURE);
+    }
+    event.events = EPOLLIN;
+    event.data.fd = listen_sock;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sock, &event) == -1) {
+        perror("epoll_ctl failed");
+        exit(EXIT_FAILURE);
+    }
+    for (;;) {
+        n = epoll_wait(epollfd, events, MAX_EVENTS, 0);
+        if (n == -1) {
+            perror("epoll_wait failed");
+            exit(EXIT_FAILURE);
+        }
+        for (i = 0; i < n; i++) {
+            if (events[i].data.fd == listen_sock) {
+                client_sock = accept4(listen_sock, NULL, NULL, SOCK_NONBLOCK);
+                if (client_sock == -1) {
+                    perror("accept failed");
+                    exit(EXIT_FAILURE);
+                }
+                event.events = EPOLLIN;
+                event.data.fd = client_sock;
+                if (epoll_ctl(epollfd, EPOLL_CTL_ADD, client_sock, &event) == -1) {
+                    perror("epoll_ctl failed");
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                handleInput(epollfd, events[i].data.fd);
+            }
+        }
+    }
+    return NULL;
 }
 
-int main() {
-	pthread_t thr1, thr2, thr3;
-	if (pthread_create(&thr1, NULL, startLoop, NULL) != 0) {
-		perror("pthread_create failed");
-		exit(EXIT_FAILURE);
-	}
-	if (pthread_create(&thr2, NULL, startLoop, NULL) != 0) {
+int main()
+{
+    pthread_t thr1, thr2, thr3;
+    if (pthread_create(&thr1, NULL, startLoop, NULL) != 0) {
         perror("pthread_create failed");
         exit(EXIT_FAILURE);
-	}
-	if (pthread_create(&thr3, NULL, startLoop, NULL) != 0) {
+    }
+    if (pthread_create(&thr2, NULL, startLoop, NULL) != 0) {
         perror("pthread_create failed");
         exit(EXIT_FAILURE);
-	}
-	pthread_join(thr1, NULL);
-	pthread_join(thr2, NULL);
-	pthread_join(thr3, NULL);
-	return 0;
+    }
+    if (pthread_create(&thr3, NULL, startLoop, NULL) != 0) {
+        perror("pthread_create failed");
+        exit(EXIT_FAILURE);
+    }
+    pthread_join(thr1, NULL);
+    pthread_join(thr2, NULL);
+    pthread_join(thr3, NULL);
+
+    return 0;
 }
--- epoll.go
+++ epoll.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"runtime"
 	"syscall"
 )
 
@@ -14,57 +13,57 @@ var DefaultResponse = []byte("HTTP/1.1 200 OK\r\n" +
 	"\r\n" +
 	"Hello World")
 
-func start_loop() {
+func startLoop() {
 	addr := &syscall.SockaddrInet4{Port: 8888}
-	listen_sock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM|syscall.SOCK_NONBLOCK, syscall.IPPROTO_TCP)
+	listenSock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM|syscall.SOCK_NONBLOCK, syscall.IPPROTO_TCP)
 	if err != nil {
 		log.Fatal("syscall.Socket:", err)
 	}
-	if err = syscall.SetsockoptInt(listen_sock, syscall.SOL_SOCKET, SO_REUSEPORT, 1); err != nil {
+	if err = syscall.SetsockoptInt(listenSock, syscall.SOL_SOCKET, SO_REUSEPORT, 1); err != nil {
 		log.Fatal("syscall.SetsockoptInt:", err)
 	}
-	if err = syscall.Bind(listen_sock, addr); err != nil {
+	if err = syscall.Bind(listenSock, addr); err != nil {
 		log.Fatal("syscall.Bind:", err)
 	}
-	if err = syscall.Listen(listen_sock, syscall.SOMAXCONN); err != nil {
+	if err = syscall.Listen(listenSock, syscall.SOMAXCONN); err != nil {
 		log.Fatal("syscall.Listen:", err)
 	}
-	epollfd, err := syscall.EpollCreate1(0)
+	epollFd, err := syscall.EpollCreate1(0)
 	if err != nil {
 		log.Fatal("syscall.EpollCreate1:", err)
 	}
-	event := syscall.EpollEvent{Fd: int32(listen_sock), Events: syscall.EPOLLIN}
-	if err = syscall.EpollCtl(epollfd, syscall.EPOLL_CTL_ADD, listen_sock, &event); err != nil {
+	event := syscall.EpollEvent{Fd: int32(listenSock), Events: syscall.EPOLLIN}
+	if err = syscall.EpollCtl(epollFd, syscall.EPOLL_CTL_ADD, listenSock, &event); err != nil {
 		log.Fatal("syscall.EpollCtl:", err)
 	}
-	events := make([]syscall.EpollEvent, 128)
+	events := make([]syscall.EpollEvent, 16384)
 	buf := make([]byte, 16384)
 	for {
-		n, err := syscall.EpollWait(epollfd, events, -1)
+		n, err := syscall.EpollWait(epollFd, events, -1)
 		if err != nil {
 			log.Fatal("syscall.EpollWait:", err)
 		}
 		for i := 0; i < n; i++ {
 			switch {
-			case events[i].Fd == int32(listen_sock):
-				client_sock, _, err := syscall.Accept4(listen_sock, syscall.SOCK_NONBLOCK)
+			case events[i].Fd == int32(listenSock):
+				clientSock, _, err := syscall.Accept4(listenSock, syscall.SOCK_NONBLOCK)
 				if err != nil {
 					log.Fatal("syscall.Accept4:", err)
 				}
-				event = syscall.EpollEvent{Fd: int32(client_sock), Events: syscall.EPOLLIN}
-				if err = syscall.EpollCtl(epollfd, syscall.EPOLL_CTL_ADD, client_sock, &event); err != nil {
+				event = syscall.EpollEvent{Fd: int32(clientSock), Events: syscall.EPOLLIN}
+				if err = syscall.EpollCtl(epollFd, syscall.EPOLL_CTL_ADD, clientSock, &event); err != nil {
 					log.Fatal("syscall.EpollCtl:", err)
 				}
-			case events[i].Fd != int32(listen_sock):
+			case events[i].Fd != int32(listenSock):
 				fd := int(events[i].Fd)
 				m, err := syscall.Read(fd, buf)
 				if m == 0 {
-					syscall.EpollCtl(epollfd, syscall.EPOLL_CTL_DEL, fd, nil)
+					syscall.EpollCtl(epollFd, syscall.EPOLL_CTL_DEL, fd, nil)
 					syscall.Close(fd)
 					continue
 				}
 				if err != nil {
-					syscall.EpollCtl(epollfd, syscall.EPOLL_CTL_DEL, fd, nil)
+					syscall.EpollCtl(epollFd, syscall.EPOLL_CTL_DEL, fd, nil)
 					syscall.Close(fd)
 					continue
 				}
@@ -78,10 +77,7 @@ func start_loop() {
 }
 
 func main() {
-	n := runtime.NumCPU()
-	for i := 0; i < n; i++ {
-		go start_loop()
-	}
-	var ch chan struct{}
-	<-ch
+	go startLoop()
+	go startLoop()
+	startLoop()
 }
